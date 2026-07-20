4f333261c8782d56ab176b7694a5919db382eec8
TheRootDaemon <harsha.manjula.venkataramanan@gmail.com>
2026-07-20 21:54:04 +0530

    cli: Add completions

---
+++ completions/tlgc.bash
@@ -0,0 +1,32 @@
+# shellcheck shell=bash
+
+_tlgc() {
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    local opts="-u -l -a -s -i -r -p -L -o -c -R -q -v -h \
+    --update --list --list-all --search --list-platforms --list-languages \
+    --info --render --clean-cache --gen-config --config-path --platform \
+    --language --short-options --long-options --edit --offline --compact \
+    --no-compact --raw --no-raw --quiet --verbose --color --config --version --help"
+
+    if [[ $cur == -* ]]; then
+        mapfile -t COMPREPLY < <(compgen -W "$opts" -- "$cur")
+        return 0
+    fi
+
+    case $prev in
+        -r|--render|--config)
+            mapfile -t COMPREPLY < <(compgen -f -- "$cur");;
+        --color)
+            mapfile -t COMPREPLY < <(compgen -W "auto always never" -- "$cur");;
+        -p|--platform)
+            mapfile -t COMPREPLY < <(compgen -W "$(tlgc --offline --list-platforms 2> /dev/null)" -- "$cur");;
+        -L|--language)
+            mapfile -t COMPREPLY < <(compgen -W "$(tlgc --offline --list-languages 2> /dev/null)" -- "$cur");;
+        *)
+            mapfile -t COMPREPLY < <(compgen -W "$(tlgc --offline --list-all 2> /dev/null)" -- "$cur");;
+    esac
+}
+
+complete -o bashdefault -F _tlgc tlgc
