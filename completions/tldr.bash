98046752277c512b66c910d173d4141996f32e18
TheRootDaemon <harsha.manjula.venkataramanan@gmail.com>
2026-08-12 18:24:02 +0530

    chore(help): Add help strings, completions

--- completions/tldr.bash
+++ completions/tldr.bash
@@ -2,10 +2,11 @@
 
 _tldr() {
     local cur="${COMP_WORDS[COMP_CWORD]}"
-    local prev="${COMP_WORDS[COMP_CWORD-1]}"
+    local prev="${COMP_WORDS[COMP_CWORD - 1]}"
 
     local opts="-u -l -a -s -b -i -r -p -L -o -c -R -q -v -h \
-    --update --list --list-all --search --browse --list-platforms --list-languages \
+    --update --list --list-all --lint --tabular \
+    --ignore --search --browse --list-platforms --list-languages \
     --info --render --clean-cache --gen-config --config-path --platform \
     --language --short-options --long-options --edit --offline --compact \
     --no-compact --raw --no-raw --quiet --verbose --color --config --version --help"
@@ -16,16 +17,21 @@ _tldr() {
     fi
 
     case $prev in
-        -r|--render|--config)
-            mapfile -t COMPREPLY < <(compgen -f -- "$cur");;
-        --color)
-            mapfile -t COMPREPLY < <(compgen -W "auto always never" -- "$cur");;
-        -p|--platform)
-            mapfile -t COMPREPLY < <(compgen -W "$(tldr --offline --list-platforms 2> /dev/null)" -- "$cur");;
-        -L|--language)
-            mapfile -t COMPREPLY < <(compgen -W "$(tldr --offline --list-languages 2> /dev/null)" -- "$cur");;
-        *)
-            mapfile -t COMPREPLY < <(compgen -W "$(tldr --offline --list-all 2> /dev/null)" -- "$cur");;
+    -r | --render | --config | --lint)
+        mapfile -t COMPREPLY < <(compgen -f -- "$cur")
+        ;;
+    --color)
+        mapfile -t COMPREPLY < <(compgen -W "auto always never" -- "$cur")
+        ;;
+    -p | --platform)
+        mapfile -t COMPREPLY < <(compgen -W "$(tldr --offline --list-platforms 2>/dev/null)" -- "$cur")
+        ;;
+    -L | --language)
+        mapfile -t COMPREPLY < <(compgen -W "$(tldr --offline --list-languages 2>/dev/null)" -- "$cur")
+        ;;
+    *)
+        mapfile -t COMPREPLY < <(compgen -W "$(tldr --offline --list-all 2>/dev/null)" -- "$cur")
+        ;;
     esac
 }
 
